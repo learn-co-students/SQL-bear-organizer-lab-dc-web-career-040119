1c088d73e8d585231938e7f04ac023a7589bd7a4
Justin <justinhwu95@gmail.com>
2019-04-14 18:43:25 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name, age, color, temperament, alive, gender) values ("Polar Bear", 5, "White", "Cool", true, "Male");
+insert into bears (name, age, color, temperament, alive, gender) values ("Black Bear", 5, "Black", "Kind of Cool", true, "Male");
+insert into bears (name, age, color, temperament, alive, gender) values ("Orange Bear", 5, "Orange", "All right", true, "Female");
+insert into bears (name, age, color, temperament, alive, gender) values ("Purple Bear", 5, "Purle", "Awesome", true, "Female");
+insert into bears (name, age, color, temperament, alive, gender) values ("Red Bear", 5, "Red", "No Bueno", true, "Male");
+  insert into bears (name, age, color, temperament, alive, gender) values ("Blue Bear", 5, "Blue", "Coolio", true, "Female");
+    insert into bears (name, age, color, temperament, alive, gender) values ("Pink Bear", 5, "Pink", "Cool Pink", true, "Male");
+      insert into bears (name, age, color, temperament, alive, gender) values (NULL, 5, "Green", "No", true, "Female")
