5c5825607a9c957f0a2d28e5d4e847d81bc42b6b
github username <github email address>
2019-06-01 20:37:54 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,132 @@
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'grizzly',
+  12,
+  'Male',
+  'Black',
+  'mean',
+  'yes'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'grizzly2',
+  12,
+  'Male',
+  'Black',
+  'mean',
+  'yes'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'grizzly3',
+  12,
+  'Male',
+  'Black',
+  'mean',
+  'yes'
+);
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'grizzly4',
+  12,
+  'Male',
+  'Black',
+  'mean',
+  'yes'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'grizzly5',
+  12,
+  'Male',
+  'Black',
+  'mean',
+  'yes'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'grizzly6',
+  12,
+  'Male',
+  'Black',
+  'mean',
+  'yes'
+);
+
+INSERT INTO bears(
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  'grizzly7',
+  12,
+  'Male',
+  'Black',
+  'mean',
+  'yes'
+);
+
+INSERT INTO bears(
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+  12,
+  'Male',
+  'Black',
+  'mean',
+  'yes'
+);
